07ea35532e921933634f13a59b9255f4565e7d46
Thomas Kloppholz <lexorius@web.de>
2025-08-13 00:50:07 +0200

    Update __init__.py

--- custom_components/ontime/__init__.py
+++ custom_components/ontime/__init__.py
@@ -2,10 +2,11 @@
   "domain": "ontime",
   "name": "Ontime Timer Control",
   "codeowners": ["@lexorius"],
-  "config_flow": true,
+  "config_flow": True,
   "dependencies": [],
   "documentation": "https://github.com/lexorius/ontime-ha",
   "iot_class": "local_polling",
   "requirements": ["aiohttp>=3.8.0"],
   "version": "1.0.0"
+
 }
